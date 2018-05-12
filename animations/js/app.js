0094a320b50586faf5542dde48f3e9e98b2a2cd8
Aditya R. Wakade <wkaditya@uw.edu>
2018-05-12 14:43:52 -0700

    Comments added
    comments

--- animations/js/app.js
+++ animations/js/app.js
@@ -1,26 +1,48 @@
+// HOW TO REFERENCE INPUT ELEMENTS FROM index.html page
+// if the id attribute of an <input> tag is "name-input" in the index.html file, I will use it in DOM functions
+// to reference e.g. document.getElementById("name-input")
 
-// id = 'name-input' holds the user name input by the user in real time
+
+
+// PART1: TAKING AN INPUT NAME FROM USER AND DISPLAYING IT AS THE USER EDITS IT
+
+// We make 2 variables called usr_nam_in and usr_nam_out which hold the input value by user and output value which we have to display
+// referencing the variables using DOM by their id in index.html tell the app that from where we have to take or display the name
 var usr_nam_in = document.getElementById("name-input");
 var usr_nam_out = document.getElementById("name-output");
 
-usr_nam_in.addEventListener("input", function(){
-    usr_nam_out.innerText = usr_nam_in.value;
-});
+
+// addEventListener helps the app.js to identify if there are any action taken by user (for more description refer: https://stackoverflow.com/questions/2695793/javascript-what-is-addeventlistener)
+// The innerText property sets the text content of the specified variable, usr_nam_out in this case.
+// The value property returns the value of the option, in this case usr_nam_in
+            usr_nam_in.addEventListener("input", function(){
+                usr_nam_out.innerText = usr_nam_in.value;
+            });
 
 
-// id = 'animation-select' it holds the value which is to be animated
-var object_to_animate = document.getElementById("welcome");
-// What to do when the animate button is clicked
+
+// Part 2: Animation options for animating the displayed text from Part 1
+
+
+// Receiving the style which the user selected to animate the displayed text (drop down list option)
+var style = document.getElementById("animation-select").value;
+
+// Option to hold the animate button
 var click_animate = document.getElementById("animate");
+
+
+// What to animate? once the style is selected and the animation button is clicked
+var object_to_animate = document.getElementById("welcome");
 
 // EVENT LISTENER: What happens when you click the animate button
 click_animate.addEventListener("click", function() {
-    // Receiving the style which the user selected to animate the object
-    var style = document.getElementById("animation-select").value;
-    // Triggering animation
+// Writing the animation function
+
+//add the selected style to the object to be animated
     object_to_animate.classList.add(style);
+
     // Removing animated after one click
-    object_to_animate.addEventListener("animationend", function() {
-        object_to_animate.classList.remove(style);
-    }, {once: true});
+    //object_to_animate.addEventListener("animationend", function() {
+      //  object_to_animate.classList.remove(style);
+    //}, {once: true});
 });
