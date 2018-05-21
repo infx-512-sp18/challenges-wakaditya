5df57fd0e5029b45aa1b0baed342d7e434ae7c71
Aditya R. Wakade <wkaditya@uw.edu>
2018-05-20 23:00:44 -0700

    test
    test

--- movies/js/app.js
+++ movies/js/app.js
@@ -1,53 +1,79 @@
+var userString = document.getElementById('movie-filter');
+console.log (userString.textContent);
+
+function updateMovies() {
+    console.log (userString.value);
+    console.log (createNewList(MOVIES, userString.value))
+    var updatedMovieList = (createNewList(MOVIES, userString.value));
+    console.log (renderMovies (updatedMovieList));
+}
+
+//changes query to lowercase, then filters whether it matches the criteria in filterMovie, which is whether the string provided by user exists in the title
+function createNewList (MOVIES, query) { 
+    // filterMovie(MOVIES, query)
+    var string = query.toLowerCase();
+    var newMovies = MOVIES.filter( function(movie) {
+        var lowerMovie = movie.title.toLowerCase();
+        if ((lowerMovie.indexOf(string) !== -1)) { 
+            return true;
+        }
+        if ((lowerMovie.indexOf(string) === -1)) {
+            return false;
+        }})
+    return newMovies;
+}
+//when event input occurs by the user, the input (userString) triggers createNewList using the user string to search. 
+userString.addEventListener("input", updateMovies);
+
+
+//Display & sort data
+
 function renderMovies(movie_list) {
+    //select the <tbody> element
     var tbody = document.querySelector("tbody");
-    
+    //Sort data by sales before passing in
+    function compare(record1, record2) {
+        return record2.revenue-record1.revenue;
+      }
+    movie_list.sort(compare);
+    //clear any existing content in the body
     tbody.textContent = "";
-
-    for (var idx = 0; idx < movie_list.length; idx++) {
-        var movie = movie_list[idx];
-        
-        tbody.appendChild(renderMovie(movie))
+    //for each element in the array...
+    for (var i = 0; i < movie_list.length; i++) {
+        //get the movie record at the current index
+        var movie = movie_list[i];
+        //render that movie record as a <tr> with <td>s
+        //and append it to the <tbody>
+        tbody.appendChild(renderMovie(movie));
     }
-
 }
 
 function renderMovie(movie) {
+    //create the <tr> element
     var tr = document.createElement("tr");
-    
-    tr.appendChild(renderMovieProp(movie.title));
+    //create and append the <td> elements
+    tr.appendChild(renderMovieProp(movie.title, true));
     tr.appendChild(renderMovieProp(movie.revenue));
     tr.appendChild(renderMovieProp(movie.rating));
-    
-    return tr;
-}
+     //return the table row to the caller
+     return tr;
+
+    }
 
 function renderMovieProp(content, nonNumeric) {
+    //create new td element
     var td = document.createElement("td");
-    
+    //add element title, revenue, rating, to the new td element \
     td.textContent = content;
-
-    if (nonNumeric) {
-       td.classList.add("non-numeric")
-}
-    return td;
-}
-
-MOVIES.sort(function(record1, record2) {
-    return record2.revenue - record1.revenue;
-
-});
-
-var movieInput = document.getElementById('movie-filter');
-
-movieInput.addEventListener("input", function filterItems(query) {
-    return MOVIES.title.filter(function(el) {
-        return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    //if it should be formatted as nonnumeric
+    if (td.querySelector("non-numeric") == true) {
+        //add the "numeric" style class
+        td.classList.add("non-numeric");
+   }
+   if (nonNumeric) {
+    //add the "numeric" style class
+    td.classList.add("non-numeric");
     }
-) 
-
-});
-
-
-
-renderMovies(movie);
-
+    //return new element
+    return td;
+} 
