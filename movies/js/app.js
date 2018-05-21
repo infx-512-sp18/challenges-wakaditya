4179e708db5b52474f416d1f156f5f01024d7803
Aditya R. Wakade <wkaditya@uw.edu>
2018-05-20 22:56:17 -0700

    sample
    function code

--- movies/js/app.js
+++ movies/js/app.js
@@ -1,11 +1,53 @@
 function renderMovies(movie_list) {
+    var tbody = document.querySelector("tbody");
+    
+    tbody.textContent = "";
+
+    for (var idx = 0; idx < movie_list.length; idx++) {
+        var movie = movie_list[idx];
+        
+        tbody.appendChild(renderMovie(movie))
+    }
 
 }
 
 function renderMovie(movie) {
-
+    var tr = document.createElement("tr");
+    
+    tr.appendChild(renderMovieProp(movie.title));
+    tr.appendChild(renderMovieProp(movie.revenue));
+    tr.appendChild(renderMovieProp(movie.rating));
+    
+    return tr;
 }
 
 function renderMovieProp(content, nonNumeric) {
+    var td = document.createElement("td");
+    
+    td.textContent = content;
 
+    if (nonNumeric) {
+       td.classList.add("non-numeric")
 }
+    return td;
+}
+
+MOVIES.sort(function(record1, record2) {
+    return record2.revenue - record1.revenue;
+
+});
+
+var movieInput = document.getElementById('movie-filter');
+
+movieInput.addEventListener("input", function filterItems(query) {
+    return MOVIES.title.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    }
+) 
+
+});
+
+
+
+renderMovies(movie);
+
